9993a0953be424a2d0cc10ea11aafbbbb17e8cf5
yuvalpiwix <yuvalpi@wix.com>
2024-02-24 22:34:29 +0200

    dropdown menu responive

--- Comp/Navbar.jsx
+++ Comp/Navbar.jsx
@@ -1,11 +1,20 @@
 import { NavbarItem } from "./NavbarItem.jsx";
 export function Navbar() {
-  const navbarItem = ["journal", "about", "work", "contact"];
+  const navbarItem = [
+    {
+      item: "journal",
+      subItems: ["October 2016", "January 2017", "April 2017", "July 2017"],
+    },
+    { item: "about" },
+    { item: "work" },
+    { item: "contact" },
+  ];
+  // const navbarItem = ["journal", "about", "work", "contact"];
   return (
     <nav>
       <ul className="clean-list">
         {navbarItem.map((item, idx) => (
-          <NavbarItem key={idx} name={item} />
+          <NavbarItem key={idx} item={item} />
         ))}
       </ul>
     </nav>
--- Comp/NavbarItem.jsx
+++ Comp/NavbarItem.jsx
@@ -1,9 +1,28 @@
-export function NavbarItem({ name }) {
+export function NavbarItem({ item }) {
+  const listClass = `navbar-item ${item.subItems ? "parent-item" : ""}`;
   return (
-    <li className={`navbar-item ${name == "journal" ? "active" : ""}`}>
+    // <li className={`navbar-item ${item.item == "journal" ? "active" : ""}`}>
+    <li className={listClass}>
       <a href="" className="navbar-link">
-        {name}
+        {item.item}
       </a>
+      {item.subItems && <SubItemsMenu subItems={item.subItems} />}
     </li>
+  );
+}
+
+export function SubItemsMenu({ subItems }) {
+  return (
+    <React.Fragment>
+      <label htmlFor="journal-subitems"></label>
+      <input type="checkbox" name="journal-subitems" id="journal-subitems" />
+      <ul className="clean-list sub-list">
+        {subItems.map((item, idx) => (
+          <li key={idx}>
+            <a href="">{item}</a>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
   );
 }
